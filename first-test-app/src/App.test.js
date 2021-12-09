71e78faae13d2abf0a5bcf28df4b29fe9fd3d5e5
jhLim97 <wnsgur2749@naver.com>
2021-12-09 19:05:06 +0900

    test: tdd 기반으로 카운터 숫자 및 플러스, 마이너스 버튼 생성관련 기능 구현

--- first-test-app/src/App.test.js
+++ first-test-app/src/App.test.js
@@ -1,8 +1,20 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('the counter starts at 0', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent(0);
+});
+
+test('minus button has correct text', () => {
+  render(<App />);
+  const minusButtonElement = screen.getByTestId("minus-button");
+  expect(minusButtonElement).toHaveTextContent("-");
+});
+
+test('plus button has correct text', () => {
+  render(<App />);
+  const plusButtonElement = screen.getByTestId("plus-button");
+  expect(plusButtonElement).toHaveTextContent("+");
 });
