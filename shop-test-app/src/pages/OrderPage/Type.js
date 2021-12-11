720b30d688b57726e87835930e0b5411f752af7d
jhLim97 <wnsgur2749@naver.com>
2021-12-11 18:49:30 +0900

    feat: 서버 요청 후 에러발생 시 배너 노출 비즈니스 로직 구현

--- shop-test-app/src/pages/OrderPage/Type.js
+++ shop-test-app/src/pages/OrderPage/Type.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Products from './Products';
+import Options from './Options';
+import ErrorBanner from '../../components/ErrorBanner';
 
 function Type({ orderType }) {
     const [items, setItems] = useState([]);
+    const [error, setError] = useState(false);
 
     useEffect(() => {
         loadItems(orderType);
@@ -14,11 +17,15 @@ function Type({ orderType }) {
             let response = await axios.get(`http://localhost:5000/${orderType}`);
             setItems(response.data);
         } catch (error) {
-            console.log(error);
+            setError(true);
         }
     }
 
-    const ItemComponents = orderType === "products" ? Products : null;
+    if (error) {
+        return <ErrorBanner message="에러가 발생했습니다." />;
+    }
+
+    const ItemComponents = orderType === "products" ? Products : Options;
 
     const optionItems = items.map((item) => (
         <ItemComponents
