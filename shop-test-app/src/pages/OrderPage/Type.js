0db0f013cee5b818e83d4b95218de22aadd78973
jhLim97 <wnsgur2749@naver.com>
2021-12-11 19:41:01 +0900

    feat: 실제 UI를 위한 코드 작성

--- shop-test-app/src/pages/OrderPage/Type.js
+++ shop-test-app/src/pages/OrderPage/Type.js
@@ -36,9 +36,17 @@ function Type({ orderType }) {
     ));
 
     return (
-        <div>
-            {optionItems}
-        </div>
+        <>
+            <h2>주문 종류</h2>
+            <p>하나의 가격</p>
+            <p>총 가격:</p>
+            <div
+                style={{
+                    display: "flex",
+                    flexDirection: orderType === "options" && "column",
+                }}
+            >{optionItems}</div>
+        </>
     )
 }
 
